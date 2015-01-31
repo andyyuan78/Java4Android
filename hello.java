0e9a2e4c40d42fb4f0247c476c5de05cfe47e0c2
andyyuan78 <andyyuan78@gmail.com>
2015-01-31 16:31:30 +0800

    add hello.java
    and how to config javac!

--- hello.java
+++ hello.java
@@ -1,2 +1,5 @@
-public class Hello{
+public class hello{  // hello must be the same with the file name : case sensitive, Hello is not OK
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
 }
