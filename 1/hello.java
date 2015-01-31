6002401ed535945289407ead7865c1d21823da95
andyyuan78 <andyyuan78@gmail.com>
2015-01-31 17:11:02 +0800

    updated

--- 1/hello.java
+++ 1/hello.java
@@ -1,4 +1,4 @@
-public class hello{  // hello must be the same with the file name : case sensitive, Hello is not OK
+public class Hello{  // hello must be the same with the file name : case sensitive, hello is not OK
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
